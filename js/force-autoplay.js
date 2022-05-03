f3113b08bb47c706d064c8e966e0905d071ec9b6
共和国工科生 <1335075427@qq.com>
2022-05-03 17:33:52 +0800

    更新博客

--- js/force-autoplay.js
+++ js/force-autoplay.js
@@ -1,11 +1,12 @@
 let ForceAutoplay = (Event) =>
 {
-    Event.preventDefault();
-    Event.stopPropagation();
     window.removeEventListener("keydown", ForceAutoplay);
     window.removeEventListener("click", ForceAutoplay);
     document.querySelector("div.aplayer-button.aplayer-play").click();
 };
 
-window.addEventListener("keydown", ForceAutoplay);
-window.addEventListener("click", ForceAutoplay);
+window.addEventListener("load", () =>
+{
+    window.addEventListener("keydown", ForceAutoplay);
+    window.addEventListener("click", ForceAutoplay);
+});
